f5394cfa7bf2548b4068c160ee0f06a97ced8fb7
shandrolis <joepvdstaai@live.nl>
2016-04-30 18:04:30 +0200

    added readAssetIntoString to Parser

--- app/src/main/java/nl/saxion/joep/twetter/Model/JSONParser.java
+++ app/src/main/java/nl/saxion/joep/twetter/Model/JSONParser.java
@@ -1,7 +1,49 @@
 package nl.saxion.joep.twetter.Model;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by joepv on 30.apr.2016.
  */
 public class JSONParser {
+
+
+    /**
+     * Reads an asset file and returns a string with the full contents.
+     *
+     * @param filename The filename of the file to read.
+     * @return The contents of the file.
+     * @throws IOException If file could not be found or not read.
+     */
+    public static String readAssetIntoString(Context context, String filename) throws IOException {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            InputStream is = context.getAssets().open(filename, AssetManager.ACCESS_BUFFER);
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
 }

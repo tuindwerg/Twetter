66e24658389d799fa52b0e55af4e86e1c90ffecd
shandrolis <joepvdstaai@live.nl>
2016-05-13 15:33:00 +0200

    read asset into string

--- app/src/main/java/nl/saxion/joep/twetter/Activities/MainActivity.java
+++ app/src/main/java/nl/saxion/joep/twetter/Activities/MainActivity.java
@@ -2,7 +2,11 @@ package nl.saxion.joep.twetter.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.IOException;
+
+import nl.saxion.joep.twetter.Model.JSONParser;
 import nl.saxion.joep.twetter.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            JSONParser.readAssetIntoString(this,"tweets.json");
+        }catch (IOException ioE){
+            Log.e("ERROR","IOException: read asset into string");
+        }
+
+
     }
 }
